0b7e18d9a8fedb53a33a66e420644c1c7f87a47a
nagoth <naggomezart@unal.edu.co>
2020-05-22 16:23:07 -0500

    Programa hecho.md

---
+++ guessinggame.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# File: guessinggme.sh
+
+function create {
+	touch {1..97}.txt  #Creo 100 archivos de texto
+}
+create
+no=0
+num=$(ls | wc -l)
+echo "Guess how many files are in this directory, $num"
+
+while [[ no -eq 0 ]]
+do
+
+read res
+
+	if [[ $res -lt $num ]]
+	then
+		echo " $res is too low, try again"
+	fi
+
+	if [[ $res -gt $num ]]
+	then
+		echo "$res is to high, try again"
+	fi
+
+	if [[ $res -eq $num ]]
+	then
+	no=1
+		echo " You Guessed"
+	fi
+done
+rm *.txt  #Remuevo todos los archivos de texto
+
--- makefile
+++ makefile
@@ -3,7 +3,7 @@ README.md:
 	echo "" >> README.md
 	echo "## This is a smaller heading" >> README.md
 	echo "" >> README.md
-	echo date +%c >> README.md
+	date +%c >> README.md
 	echo "And as **imagination** bodies forth, The forms of things *unknown*, the poet’s pen, Turns them to shapes and gives to airy nothing, A local *habitation* and a **name**." >> README.md
 	echo "" >> README.md
 	echo "Here is how you make [a link](https://www.wikipedia.org/)." >> README.md
